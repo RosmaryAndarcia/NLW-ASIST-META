9ab474b3ad53a5d488b735c2eb1b44aeba889cbe
Rosmary Andarcia <rosmaryand0306@gmail.com>
2025-07-11 12:09:43 -0300

    feat: iniciar proyecto

---
+++ README.md
@@ -0,0 +1,133 @@
+# HTML
+
+- HyperText
+  - Texto puros, livros, revistas, jornais
+  - Links: acessar outros documentos
+  - imagens, videos, audios
+- Markup
+  - Marcaçao
+  - Tags: <a> Link </a>
+  - Atributos <a href="https://rockeseat.com.br">
+  - Globais: id, class, ...
+- Language
+  - Linguagem
+  - Sintaxe: maneira de escrever
+
+# HTTP
+
+- HyperText
+- Transfer
+  - Transferencia
+- Protocol 
+  - Protocolo
+  - Conjunto de regras
+- Methods HTTP (verbos): GET, Post, Patch/Put, Delete
+- Header (cabeçalhos): Instruçoes extras para cada chamada
+
+# URL
+
+- Uniform
+  - Uniforme
+- Resource
+  - Recurso
+- Locator
+  - Localizador
+  Encontrar um recurso (html, css, js, pdf, png, jpg, mp4, mp3 ...)
+
+# IP
+
+- Internet
+  - Rede mundial de computadores
+- Protocol
+  - Conjunto de regras
+Endereço do computador
+rocketseat.com.br (dominio)
+123.32.1.23 (ip)
+
+# DNS
+
+- Domain
+  - Dominio
+- Name
+  - Nome 
+- Server
+ - Servidor
+
+ # CSS
+
+- Cascading
+  - Cascata
+  - Regras das escritas
+  - Hierarquia
+  - Especificidade
+- Style
+  -Estilo
+- Sheet
+  - Folha
+
+De um arquivo que tera na sua sintaxe, declaraçoes, propiedades e valores.
+A partir disso, o HTML é impactado visualmente.
+
+- declaraçao
+- seletor
+- propiedade e valor 
+
+# JS
+
+- Linguagem de programaçao
+- Browsers
+- Input -> Process -> Output
+- Variaveis
+- Funtion
+  - Agrupamento e reuso de codigo
+  - Sequencia logica 
+  - Saida
+- Gemini (IA): esperar uma reposta
+- Mexer no  meu navegador
+- Estrutura de dados
+- Estrutura de decisão
+- Algoritmo: sequencia de passos logica, de maneira ordena, a fim de chegar a alguma conclusão (limitada)
+- Logica (Pensamento computacional)
+  - Abstração
+  - Decomposição
+  - Algoritmo
+  - Reconhecimento de padrão
+- Tipos de dados
+  - Number (numeros)
+  - String (textos) ("", '', ``)
+  - Boolean: true | false
+  - Objet
+
+#DOM
+
+- Document
+- Objet
+- Model
+
+#API
+- Application
+- Programming
+- Interface
+- JSON: javaScript Object Notation
+
+# CDN
+- Content
+- Delivery
+- Network
+
+# LLM
+- Large
+- Language
+- Model
+- Gemini, OpenAI (chatGPT), Anthropic (Claude) ...
+
+#Agentes AI
+
+- Tools (ferramentas): permitem uso de codigo, ou apps, ou qualquer informação extra
+- Melhor contexto
+
+# Engenharia de prompt
+
+- One shot: uma unica pergunta sem muito contexto
+- Few shot: é apresentado exemplos do que se espera
+- Chain of Thought: cadeia de pensamento para a IA responder gradativamente conforme a instrução.
---
+++ script.js
@@ -0,0 +1,96 @@
+const apiKeyInput = document.getElementById("apiKey")
+const gameSelect = document.getElementById("gameSelect")
+const questionInput = document.getElementById("questionInput")
+const askButton = document.getElementById("askButton")
+const aiResponse = document.getElementById("aiResponse")
+const form = document.getElementById("form")
+
+const markdownToHtml = (text) => {
+  const converter = new showdown.Converter()
+  return converter.makeHtml(text)
+}
+
+
+//AIzaSyCdX04rX0yI4XZdafw7MadQvgnhbWb1WPk
+const perguntarAI = async (apiKey, game, question) => {
+  const model = "gemini-2.0-flash"
+  const geminiUrl = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+  const pergunta = `
+    ## Especialidade
+    Voce é um especialista assitente de meta para o jogo ${game}
+    
+    ## Tarefa
+    Voce debe responder as perguntas do usuario com base no seu conhecimento do jogo, estrategias, builds e dicas
+    
+    ## Regras
+    - Se voce não sabe a resposta, responda com "Não sei" e nao tente inventar uma resposta.
+    - Se a pergunta não esta relacionada ao jogo, responda com "essa pergunta no esta relacionada ao jogo".
+    - Considere a data atual ${new Date().toLocaleDateString()} e faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coherente.
+    - Nunca responda itens que voce não tenha certeza de que existe no patch atual.
+
+    ## Resposta
+    - Economice na resposta, seja direto e responda no minimo 500 caracteres. 
+    - Responda em markdown.
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que usuario esta querendo.
+
+    ## Exemplo de resposta
+    Pergunta do usuario: Melhor build rengar jungle 
+    resposta: A build mais atual é: \n\n **Itens:** \n\n coloque os itens aqui. \n\n **Runas:** \n\n exemplo de runa \n\n
+
+    ---
+    Aqui esta a pergunta do usuario: ${question}
+`
+  const contents = [{
+    role: "user",
+    parts: [{
+      text: pergunta
+    }]
+  }]
+
+  const tools = [{
+    google_search: {}
+  }]
+
+  //chamada API
+  const response = await fetch(geminiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      contents
+    })
+  })
+  const data = await response.json()
+  return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
+  event.preventDefault()
+  const apiKey = apiKeyInput.value
+  const game = gameSelect.value
+  const question = questionInput.value
+
+  if (apiKey == "" || game == "" || question == "") {
+    alert("Por favor, preencha todos os campos")
+    return
+  }
+  askButton.disabled = true; // desabilita o botão de enviar
+  askButton.textContent = "Perguntando..."
+  askButton.classList.add("loading")
+
+  try {
+    //perguntar para a IA
+    const text = await perguntarAI(apiKey, game, question)
+    aiResponse.querySelector(".response-content").innerHTML = markdownToHtml(text)
+    aiResponse.classList.remove("hidden")
+  } catch (error) {
+
+  } finally {
+    askButton.disabled = false
+    askButton.textContent = "Perguntar"
+    askButton.classList.remove("loading")
+
+  }
+}
+form.addEventListener("submit", enviarFormulario)
